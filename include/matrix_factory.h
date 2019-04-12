0603b394bc95909163002c28caeff5fc8f1f1ecf
sheltongabe <sheltongabe0@gmail.com>
2019-04-12 10:14:00 -0400

    Actually add these files to some source control

---
+++ include/matrix_factory.h
@@ -0,0 +1,57 @@
+/**
+ *  @file	matrix_factory.h
+ *  @brief	
+ *  
+ * 	Details
+ *  
+ *  @author	First_Last_Name	username
+ *  @date	08-04-2018
+ *  @version	0.0
+ */
+
+#ifndef MATRIXFACTORY_H
+#define MATRIXFACTORY_H
+
+
+
+/**
+ * 	@class		MatrixFactory
+ * 	@brief		
+ * 
+ * 	Details
+ * 
+ */
+class MatrixFactory {
+	public:
+		/**
+		 * 	@brief	Default Constructor
+		 * 
+		 * 	Details
+		 * 
+		 * 	@version	0.0
+		 */
+		MatrixFactory();
+
+		/**
+		 * 	@brief	Copy Constructor
+		 * 
+		 * 	Details
+		 * 
+		 * 	@version	0.0
+		 */
+		MatrixFactory(MatrixFactory& copy);
+
+		/**
+		 * 	@brief	Destructor
+		 * 
+		 * 	Details
+		 * 
+		 * 	@version	0.0
+		 */
+		~MatrixFactory();
+
+	private:
+
+};
+
+#endif
---
+++ include/square_matrix.cpp
@@ -0,0 +1,33 @@
+/**
+ *  @file	square_matrix.cpp
+ *  @brief	
+ *  
+ * 	Details
+ *  
+ *  @author	First_Last_Name	username
+ *  @date	08-04-2018
+ *  @version	0.0
+ */
+
+#include "square_matrix.h"
+
+//
+// Default Constructor
+//
+SquareMatrix::SquareMatrix() {
+
+}
+
+//
+// Copy Constructor
+//
+SquareMatrix::SquareMatrix(SquareMatrix& copy) {
+
+}
+
+//
+// Destructor 
+//
+SquareMatrix::~SquareMatrix() {
+
+}
